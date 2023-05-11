8d78e4220da71ac894dbd332e29589902bab71ec
unknown <mahmoudlove1218@gmail.com>
2023-05-11 15:53:54 +0200

    new update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
         $this->call(PermissionTableSeeder::class);
         // $user = User::create([
         //     'name' => 'Adminstrator',
